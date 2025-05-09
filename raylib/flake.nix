862f1503803801be83f288effd55b16ffbc480a4
N7Art <n7141983@gmail.com>
2025-05-09 21:47:02 +0300

    п'ятниця, 9 травня 2025 21:47:02 +0300

---
+++ raylib/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "raylib flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+  outputs =
+    {self,  nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs =
+          (with pkgs; [
+            wayland-scanner
+            wayland
+            libxkbcommon
+            libGLX
+            zig
+          ])
+          ++ (with pkgs.xorg; [
+
+            libX11
+            libXcursor
+            libXext
+            libXfixes
+            libXi
+            libXinerama
+            libXrandr
+            libXrender
+          ]);
+
+      };
+    };
+}
