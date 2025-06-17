098e2f51f4105eb9b99d5d14d090cdd675e4fb47
N7Art <n7141983@gmail.com>
2025-06-18 02:11:55 +0300

    added dvui flake

---
+++ dvui/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "dvui flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    raylib-flake.url = "github:N7Art/flakes?dir=raylib";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      raylib-flake,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      deps = raylib-flake.raylibBuildInputs;
+      devShell = pkgs.mkShell {
+        buildInputs = deps;
+
+      };
+
+    in
+    {
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = deps;
+
+      };
+
+      devShell = devShell;
+    };
+}
