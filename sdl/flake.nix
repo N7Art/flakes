1724373a92d249eaa1c2b02b63bd1c9e57aa4d48
N7Art <n7141983@gmail.com>
2025-07-08 19:24:17 +0300

    added dvui flake

---
+++ sdl/flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "dvui flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  };
+
+  outputs = { self, nixpkgs }: let
+    
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      deps = 
+          (with pkgs; [
+#            wayland-scanner
+#            wayland
+#            libxkbcommon
+#            libGLX
+            sdl3
+          ])
+          ++ (with pkgs.xorg; [
+
+#            libX11
+#            libXcursor
+#            libXext
+#            libXfixes
+#            libXi
+#            libXinerama
+#            libXrandr
+#            libXrender
+          ]);
+    in
+    {
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = deps;
+
+      };
+
+      dvuiDeps = deps;
+    };
+
+
+}
